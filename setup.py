ce20c83764bdce3516556b9852bcd92a2a4d3a71
Jeremy Kelley <jeremy@collectivelabs.com>
2011-12-31 02:57:31 -0600

    added simple setup.py script for easy installation

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='databag',
+    version='0.1.0',
+    description='Put your data in a bag and get it back out again',
+    author='Jeremy Kelley',
+    author_email='jeremy@33ad.org',
+    url='https://github.com/nod/databag',
+	py_modules=['databag'],
+    )
